6efc178638bbdbdbfa6afbf0b17e367300919ee0
peacemaker07(Yuji Chino) <au4iu6@bma.biglobe.ne.jp>
2014-01-01 12:10:59 +0900

    first commit

---
+++ LED_Light_Dark/sketch_nov23a/sketch_nov23a.ino
@@ -0,0 +1,25 @@
+#define LED_PIN9 9
+#define LED_PIN10 10
+
+void setup(){
+  pinMode(LED_PIN9, OUTPUT);
+  pinMode(LED_PIN10, OUTPUT);
+}
+
+void loop(){
+  int i;
+
+  // test  
+  //だんだん光る
+  for(i = 0; i < 256; i++){
+    analogWrite(LED_PIN9, i);
+    analogWrite(LED_PIN10, 255-i);
+    delay(10);
+  }
+  //だんだん消える
+  for(i = 255; i > 0; i--){
+    analogWrite(LED_PIN9, i);
+    analogWrite(LED_PIN10, 255-i);
+    delay(10);
+  }
+}
